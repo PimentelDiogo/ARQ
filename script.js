4627f5f797b12e6186dda50f6fbe711ad3e6107e
Diogo Pimentel <d.vpimentel93@gmail.com>
2022-05-16 10:44:41 -0300

    add JS e Animação CSS

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ function initTab(){
     }
 }
 initTab();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion(){
+this.classList.toggle('ativo');
+this.nextElementSibling.classList.toggle('ativo');
+}
+accordionList.forEach((item) =>{
+    item.addEventListener('click' , activeAccordion);
+})
